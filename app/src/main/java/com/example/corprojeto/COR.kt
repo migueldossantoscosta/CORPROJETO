ebc1701307d1afd22b0242b9d2d1ef99957edb61
Miguel dos Santos Costa <miguelcostinha521@gmail.com>
2025-06-19 14:30:40 +0100

    Criação de um Pointer que lê a cor do pixel selecionado e mostra-o numa caixa

--- app/src/main/java/com/example/corprojeto/COR.kt
+++ app/src/main/java/com/example/corprojeto/COR.kt
@@ -1,25 +1,67 @@
 package com.example.corprojeto
 
 import androidx.compose.foundation.Image
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asAndroidBitmap
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.*
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
 
+
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun COR (){
+
+    var CorEscolhida by remember { mutableStateOf(Color.White) }
+    var TamanhoImagem by remember { mutableStateOf(IntSize.Zero) }
+    val contexto = LocalContext.current
+    val Imagem = ImageBitmap.imageResource(contexto.resources,R.drawable.color_wheel)
+    val bitmap = Imagem.asAndroidBitmap()
+    var CodigoCor by remember { mutableStateOf("#FFFFFF") }
+
     Image(
         //Carrega a imagem
-        painter = painterResource(id = R.drawable.color_wheel),
+        bitmap = Imagem,
         //Descreve a imagem (IMPORTANTE)
         contentDescription = "Roda com Cores",
         //Define o tamanho e a posição da imagem
         modifier = Modifier
             .fillMaxWidth() //Ocupa a largura disponivel
-            .height(300.dp), //Fixa a altura da imagem em 300dp
+            .height(300.dp) //Fixa a altura da imagem em 300dp
+            .onGloballyPositioned { coordinates -> TamanhoImagem = coordinates.size }
+            .pointerInteropFilter {event ->
+                val xRatio = event.x / TamanhoImagem.width.toFloat()
+                val yRatio = event.y / TamanhoImagem.height.toFloat()
+
+                val xBitmap = (xRatio * bitmap.width).toInt()
+                val yBitmap = (yRatio * bitmap.height).toInt()
+
+                if (xBitmap in 0 until bitmap.width && yBitmap in 0 until bitmap.height)  {
+                    val pixel = bitmap.getPixel(xBitmap, yBitmap)
+                    val Novacor = Color(pixel)
+                    CorEscolhida = Novacor
+                    CodigoCor = String.format("#%06X", 0xFFFFFF and pixel)
+                }
+                true
+            },
+
         contentScale = ContentScale.Fit
+    )
+
+    Box(
+        modifier = Modifier
+            .size(100.dp)
+            .background(CorEscolhida)
     )
 }
 
