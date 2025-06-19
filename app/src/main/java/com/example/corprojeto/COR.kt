89830335a1df71a24173b37b9c862c840a5f7aa0
Miguel dos Santos Costa <miguelcostinha521@gmail.com>
2025-06-19 02:30:48 +0100

    Mostrar a Roda de Cores

--- app/src/main/java/com/example/corprojeto/COR.kt
+++ app/src/main/java/com/example/corprojeto/COR.kt
@@ -1,2 +1,22 @@
 package com.example.corprojeto
 
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.*
+
+@Composable
+fun COR (){
+    Image(
+        painter = painterResource(id = R.drawable.color_wheel),
+        contentDescription = "Roda com Cores",
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(300.dp),
+        contentScale = ContentScale.Fit
+    )
+}
+
